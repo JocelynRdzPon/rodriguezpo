cf7a9affd3f885a9de0823a68ad98ad437fb25ef
JocelynRdzPon <jocerdzp26@gmail.com>
2021-11-21 22:23:06 -0600

    Entrega PHP PDO

---
+++ Parcial 3/PHPPDO/consultar.php
@@ -0,0 +1,47 @@
+<?php
+include "conectar.php";
+
+try {
+       $queryStr="select * from usuarios";
+       $query=$con->prepare($queryStr);
+       $query->execute();
+
+        while ($row = $query->fetch()) {
+            echo $row['idUsuario'].'-'.
+                 $row['nombre'].'-'.
+                 $row['contraseña'].'<br>';
+        }
+        $query->closeCursor();
+
+} catch(PDOException $e) {
+        echo "Error de consulta a la base de datos";
+        echo $e->getMessage();
+}
+
+try {
+    $queryStr="select * pedidos";
+    $query=$con->prepare($queryStr);
+    $query->execute();
+
+     while ($row = $query->fetch()) {
+         echo $row['idpedido'].'-'.
+              $row['idusuario'].'-'.
+              $row['nombrecliente'].'-'.
+              $row['apellidoscliente'].'-'.
+              $row['telefono'].'-'.
+              $row['direccion'].'-'.
+              $row['fechaentrega'].'-'.
+              $row['horaentrega'].'-'.
+              $row['saborpastel'].'-'.
+              $row['rellenopastel'].'-'.
+              $row['coberturapastel'].'-'.
+              $row['porcionespastel'].'<br>';
+     }
+     $query->closeCursor();
+
+} catch(PDOException $e) {
+     echo "Error de consulta a la base de datos";
+     echo $e->getMessage();
+}
+
+?>
