e39da8d23fdfc49d2a982df8cb380dbaf7acea6c
JocelynRdzPon <jocerdzp26@gmail.com>
2021-11-24 07:18:01 -0600

    Entrega Script sql

--- Parcial 3/PHPPDO/consultar.php
+++ Parcial 3/PHPPDO/consultar.php
@@ -1,22 +1,6 @@
 <?php
 include "conectar.php";
 
-try {
-       $queryStr="select * from usuarios";
-       $query=$con->prepare($queryStr);
-       $query->execute();
-
-        while ($row = $query->fetch()) {
-            echo $row['id_usuario'].'-'.
-                 $row['nombre'].'-'.
-                 $row['contraseña'].'<br>';
-        }
-        $query->closeCursor();
-
-} catch(PDOException $e) {
-        echo "Error de consulta a la base de datos";
-        echo $e->getMessage();
-}
 
 try {
     $queryStr="select * pedidos";
